893609223ecede8163811f7e2f5fbe0b66f43db6
TuningZhou <GhostG_G@hotmail.com>
2025-05-25 06:16:10 +0800

    更新

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import App from "./App";
 import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
